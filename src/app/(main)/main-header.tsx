83cada3363c23246707a943ec23d11ba863d5906
ghughlett <ghughlett@avioconsulting.com>
2026-03-04 13:10:40 -0600

    updated the header menu positioning on Account

--- src/app/(main)/main-header.tsx
+++ src/app/(main)/main-header.tsx
@@ -14,7 +14,7 @@ export default async function MainHeader({ isLoggedIn }: { isLoggedIn: boolean }
     <>
 
       <header className=" font-app rounded font-extrabold bg-[#59cdf7] flex justify-between align-middle h-[80] md:h-[110]">
-        <HeaderImage href="/my-account" src="images/family-social-icon-only.png" title="Family Social Home" tw="h-9 w-9 md:h-15 md:w-15 pt-[15] md:h-10 md:w-10" />
+        <HeaderImage href="/my-account" src="images/family-social-icon-only.png" title="Family Social Home" tw="h-11 w-11 md:h-20 md:w-20 pt-[15] md:h-13 md:w-13" />
 
         <div className="flex justify-center ">
           <nav className="font-app font-extrabold text-xs md:text-base w-max-[100vh]">
@@ -27,12 +27,12 @@ export default async function MainHeader({ isLoggedIn }: { isLoggedIn: boolean }
               <NavBar isLoggedIn={ isLoggedIn } href="/mx-train" src="icons/games.png" title="MX Train" />
               <NavBar isLoggedIn={ isLoggedIn } href="/family-threads" src="icons/family.png" title="Family Threads" />
             </ul>
-            <main className="flex pr-10 items-center gap-x-2 gap-y-2 pt-5">
+            <main className="flex pr-10 items-center gap-x-2 gap-y-2 pt-8">
               <div>
                 <p className="font-app font-extralight text-xs md:text-base pt-1">{ email }</p>
               </div>
               <div className="h-4 w-4 pt-0 md:h-10 md:w-10 md:pt[5]">
-                <HeaderImage href="/my-account" src="icons/account.png" title="My Account" tw="h-7 w-7 pb-0 md:h-7 md:w-7" />
+                <HeaderImage href="/my-account" src="icons/account.png" title="My Account" tw="h-6 w-6 pb-0 md:h-12 md:w-12" />
               </div>
             </main>
           </nav>
